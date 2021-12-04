7575605e600a1fcd1ed19a9eef09a0a24d527547
kekovina <banderes94@gmail.com>
2021-12-04 07:38:22 +0300

    final

---
+++ build/precache-manifest.a380347d01fdb2205e7762bdfe29b9f2.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c701864393bce13c8eff61c5e548058",
+    "url": "/final-edFresh-client/index.html"
+  },
+  {
+    "revision": "6abe84a79449f331ca55",
+    "url": "/final-edFresh-client/static/css/2.cc91c9c6.chunk.css"
+  },
+  {
+    "revision": "958fafc974d333ec221b",
+    "url": "/final-edFresh-client/static/css/main.f521afc8.chunk.css"
+  },
+  {
+    "revision": "6abe84a79449f331ca55",
+    "url": "/final-edFresh-client/static/js/2.4250c94e.chunk.js"
+  },
+  {
+    "revision": "1f8299b5be7fdc9f8230865fd39e80dc",
+    "url": "/final-edFresh-client/static/js/2.4250c94e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958fafc974d333ec221b",
+    "url": "/final-edFresh-client/static/js/main.851c53a9.chunk.js"
+  },
+  {
+    "revision": "d3c04d3f0cc6b9604c36",
+    "url": "/final-edFresh-client/static/js/runtime-main.5a0dbb9a.js"
+  },
+  {
+    "revision": "097d22a03414dabb47bf6222ab384047",
+    "url": "/final-edFresh-client/static/media/0.097d22a0.svg"
+  },
+  {
+    "revision": "13af0818b66a5e5a1f43fb0094da7840",
+    "url": "/final-edFresh-client/static/media/1.13af0818.svg"
+  },
+  {
+    "revision": "1b701cb3a3f03be505806eee023e6ef0",
+    "url": "/final-edFresh-client/static/media/2.1b701cb3.svg"
+  },
+  {
+    "revision": "fb4f3aa84affd445dc7a1b4c70985b47",
+    "url": "/final-edFresh-client/static/media/3.fb4f3aa8.svg"
+  },
+  {
+    "revision": "754ca7bfae5ab1208de47d66199def8d",
+    "url": "/final-edFresh-client/static/media/4.754ca7bf.svg"
+  },
+  {
+    "revision": "fe19d7269fbd1af17d80ad15dd100366",
+    "url": "/final-edFresh-client/static/media/5.fe19d726.svg"
+  },
+  {
+    "revision": "2b11eebda48fb1b4bf56a150342cde79",
+    "url": "/final-edFresh-client/static/media/angle-bottom.2b11eebd.svg"
+  },
+  {
+    "revision": "abab27c70ac3cde0a27f354cd04d13ed",
+    "url": "/final-edFresh-client/static/media/angle-top.abab27c7.svg"
+  },
+  {
+    "revision": "429028a4dfa90abe9e270a500be2c40a",
+    "url": "/final-edFresh-client/static/media/angle-white-bottom.429028a4.svg"
+  },
+  {
+    "revision": "f0d2ba62f732e19d8288b1cf302f2a21",
+    "url": "/final-edFresh-client/static/media/angle-white-top.f0d2ba62.svg"
+  },
+  {
+    "revision": "f20a1e663e08cf5ee8cbd45482966fef",
+    "url": "/final-edFresh-client/static/media/comfort.f20a1e66.png"
+  },
+  {
+    "revision": "93cd4970b0daf70e45d5c49f0b51d0a0",
+    "url": "/final-edFresh-client/static/media/compare.93cd4970.svg"
+  },
+  {
+    "revision": "43438afd2642bda36cddfc9dd4f4285d",
+    "url": "/final-edFresh-client/static/media/eco.43438afd.png"
+  },
+  {
+    "revision": "6d9d23466bb55129f037b18bbf78d69e",
+    "url": "/final-edFresh-client/static/media/favorite.6d9d2346.svg"
+  },
+  {
+    "revision": "07d8688871f7e05ed3fa8251577ee246",
+    "url": "/final-edFresh-client/static/media/filter-icon.07d86888.svg"
+  },
+  {
+    "revision": "e856e8fabcc1b48c3fbe4bd644804f2b",
+    "url": "/final-edFresh-client/static/media/header.e856e8fa.png"
+  },
+  {
+    "revision": "99fa97d41a3524eb7f1805fbe895d1e4",
+    "url": "/final-edFresh-client/static/media/logo.99fa97d4.png"
+  },
+  {
+    "revision": "5b2ec044864b55f3f45d29e7193ba9d1",
+    "url": "/final-edFresh-client/static/media/logo_footer.5b2ec044.png"
+  },
+  {
+    "revision": "146a433ce5cffb2d8e7a250301d1832c",
+    "url": "/final-edFresh-client/static/media/mobility.146a433c.png"
+  },
+  {
+    "revision": "45c4c8dc3b62fc31d205a328d129bac7",
+    "url": "/final-edFresh-client/static/media/moscowsansregular.45c4c8dc.ttf"
+  },
+  {
+    "revision": "3980267b38c202e45a6eaf16118f0bfc",
+    "url": "/final-edFresh-client/static/media/request.3980267b.svg"
+  },
+  {
+    "revision": "8c23ab4bceb6a283b30cfe227048e6e1",
+    "url": "/final-edFresh-client/static/media/safety.8c23ab4b.png"
+  },
+  {
+    "revision": "dd38b7f0bf060af088438fc24e97eb04",
+    "url": "/final-edFresh-client/static/media/search.dd38b7f0.svg"
+  },
+  {
+    "revision": "9bfde2634eae19d6ba81cb225b15b2bd",
+    "url": "/final-edFresh-client/static/media/technology.9bfde263.png"
+  },
+  {
+    "revision": "a8298309ce75c892ee763e94512ab9b8",
+    "url": "/final-edFresh-client/static/media/test.a8298309.png"
+  },
+  {
+    "revision": "33f28e71b99660b194ad2b9c29dbb6d2",
+    "url": "/final-edFresh-client/static/media/transport-logo.33f28e71.png"
+  }
+]);
